edac40b529857012928e1bb8562d65767d2c67ae
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-11 03:52:06 +0700

    build: script and config

--- vite.config.ts
+++ vite.config.ts
@@ -14,13 +14,38 @@ export default defineConfig({
 				name: "vite-plus",
 				specifier: "vite-plus/oxlint-plugin",
 			},
+			{
+				name: "no-relative-import-paths",
+				specifier: "eslint-plugin-no-relative-import-paths",
+			},
 		],
 
 		rules: {
 			"vite-plus/prefer-vite-plus-imports": "error",
+			"no-relative-import-paths/no-relative-import-paths": [
+				"warn",
+				{ allowSameFolder: false, rootDir: `/src`, prefix: "#" },
+			],
 		},
 	},
 	staged: {
 		"*": "vp check --fix --no-error-on-unmatched-pattern",
 	},
+
+	pack: [
+		{
+			format: ["esm", "cjs"],
+			sourcemap: true,
+			unbundle: true,
+		},
+		{
+			entry: {
+				"bin/*": "bin/*.ts",
+			},
+			format: "cjs",
+			sourcemap: true,
+
+			dts: false,
+		},
+	],
 });
